0afa710765b3776e53be3a99377847a0703ace8f
rommel <rommel.pineda.git@gmail.com>
2023-08-06 16:37:17 +0800

    added POST and simple inputs

--- src/app/todo/todo/todo.component.ts
+++ src/app/todo/todo/todo.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {TodoModel} from  '../../shared/todo-model.model';
+//import first the service
 import {ApiService} from '../../services/api.service';
 
 @Component({
-  selector: 'app-todo',
+  selector: 'todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
   todo !: TodoModel[];
+  //Dependecy Injection (DI)
   constructor(private apiService: ApiService){}
   
   ngOnInit(): void {
@@ -19,4 +21,9 @@ export class TodoComponent implements OnInit {
     })
   }
 
+  update(value: string){
+    //add to database here
+    //refresh the this.todo
+console.log(value);
+  }
 }
